6a18bdb08701aae0fcdfb0864643dfb8b4d335c1
mango <mchead1@naver.com>
2022-05-24 14:53:26 +0000

    :nut_and_bolt: array의 값 출력 예제를 포인터 활용하는 방안으로 변환

--- arr.c
+++ arr.c
@@ -5,7 +5,7 @@ int main(void) {
 	int arr[10] = { 1, 2, 3, 10, 5, 4, 9, 7, 6, 8 };
 	int i, maxValue = INT_MIN;
 	for (i = 0; i < 10; i++) {
-		if (maxValue < arr[i]) maxValue = arr[i];
+		if (maxValue < *(arr + i)) maxValue = arr[i];
 	}
 	printf("max value is %d\n", maxValue);
 	return 0;
