ffe037f4e50e825cc222d1a4781d2607b06df888
choemingyu <mchead1@naver.com>
2022-06-06 17:46:05 +0900

    :cyclone: 상수로 쓰이던 부분 변수화

--- assignAsyncMemory.c
+++ assignAsyncMemory.c
@@ -2,20 +2,22 @@
 #include <stdlib.h>
 
 int main(void) {
-	int **p = (int**)malloc(sizeof(int*) * 3);
-	for (int i = 0; i < 3; i++) {
-		*(p + i) = (int*)malloc(sizeof(int) * 3);
+    int width = 3;
+    int height = 3;
+	int **p = (int**)malloc(sizeof(int*) * height);
+	for (int i = 0; i < width; i++) {
+		*(p + i) = (int*)malloc(sizeof(int) * width);
 	}
 	
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			*((p + 1) + j) = i * 3 + j;
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			*((p + 1) + j) = i * height + j;
 			printf("%d ", *((p + 1) + j));
 		}
 		printf("\n");
 	}
 	
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < height; i++) {
 		*(p + 1) = i;
 	}
 }
