9c1830d7ab5a0f6080045293b3219539ec385981
choemingyu <mchead1@naver.com>
2022-06-06 23:29:48 +0900

    :nut_and_bolt: 포인터의 배열주소 초기화 및 활용

--- pointer.c
+++ pointer.c
@@ -17,5 +17,10 @@ int main(void) {
 	
 	int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 	printAddressesOfArr(arr);
+
+    // 포인터의 배열주소 활용
+    int *d = &arr;
+    printf("%d\n", *(d + 1));
+
 	return 0;
 }
