70d474a61a83fccc9ce6e311959630d4b14cb197
choemingyu <mchead1@naver.com>
2022-06-07 00:45:10 +0900

    :nut_and_bolt: 구조체 동적할당 예제

--- structEx.c
+++ structEx.c
@@ -1,4 +1,6 @@
+#include "stdlib.h"
 #include "stdio.h"
+#include "string.h"
 
 typedef struct {
     char studentId[10];
@@ -7,11 +9,24 @@ typedef struct {
     char major[51];
 } Student;
 
-int main(void) {
-    Student s = {"1135", "망고", 1, "컴퓨터 공학과"};
-
+void printStudent(Student s) {
     printf("학번: %s\n", s.studentId);
     printf("이름: %s\n", s.name);
     printf("학년: %d\n", s.grade);
     printf("학과: %s\n", s.major);
+    printf("\n");
+}
+
+int main(void) {
+    Student s = {"1135", "망고", 1, "컴퓨터 공학과"};
+
+    printStudent(s);
+
+    Student *asyncS = malloc(sizeof(Student));
+    strcpy(asyncS->studentId, "1315");
+    strcpy(asyncS->name, "유수진");
+    asyncS->grade = 1;
+    strcpy(asyncS->major, "호텔 경영 학과");
+
+    printStudent(*asyncS);
 }
