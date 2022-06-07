6642fbe51c53aa08e2541d8c8e7ceafab062ead2
choemingyu <mchead1@naver.com>
2022-06-08 01:56:29 +0900

    :nut_and_bolt: 파일 읽기

--- fileIO.c
+++ fileIO.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 typedef struct {
@@ -5,13 +6,25 @@ typedef struct {
     int score;
 } Student;
 
-void writeFile(char *str) {
-    FILE *fp = fopen("temp.txt", "w");
+void writeFile(char *filename, char *str) {
+    FILE *fp = fopen(filename, "w");
     fprintf(fp, "%s\n", str);
     fclose(fp);
 }
 
+void readFile(char *filename) {
+    char buffer[20];
+    FILE *fp = fopen(filename, "r");
+
+    fgets(buffer, sizeof(buffer), fp);
+
+    printf("%s\n", buffer);
+    fclose(fp);
+}
+
 int main(void) {
-    writeFile("Start");
+    char *name = "temp.txt";
+    writeFile(name, "Start lol");
+    readFile(name);
     return 0;
 }
