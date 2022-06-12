60e5d8aa2655f6d7ba117d7c40ad276a98becc1f
choemingyu <mchead1@naver.com>
2022-06-12 21:12:53 +0900

    :nut_and_bolt: 전처리기 사용 예제

---
+++ temp.h
@@ -0,0 +1,12 @@
+//
+// Created by 최민규 on 2022/06/12.
+//
+
+#ifndef C_CPP_SCRATCH_TEMP_H
+#define C_CPP_SCRATCH_TEMP_H
+
+int add(int a, int b) {
+    return a + b;
+}
+
+#endif //C_CPP_SCRATCH_TEMP_H
---
+++ useHeader.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "temp.h"
+
+int main(void) {
+    int sum = add(1, 2);
+    printf("sum result is %d\n", sum);
+    return 0;
+}
