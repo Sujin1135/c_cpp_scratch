7fc67202c74a04018d80a7560f36e81d9bc120c1
choemingyu <mchead1@naver.com>
2022-06-18 14:06:48 +0900

    :sparkles: linked list methods

--- linkedList.c
+++ linkedList.c
@@ -7,27 +7,57 @@ typedef struct {
     struct Node *next;
 } Node;
 
-Node *head;
-
 Node* assign() {
-    return (Node*)malloc(sizeof(Node));
+    Node * node =(Node*)malloc(sizeof(Node));
+    node->next = NULL;
+    return node;
+}
+
+void addFront(Node *root, int data) {
+    Node *node = assign();
+    Node *next = root->next;
+    node->data = data;
+    node->next = next;
+    root->next = node;
+}
+
+void removeFront(Node *node) {
+    Node *target = node->next;
+    node->next = target->next;
+    free(target);
+}
+
+void showAll(Node *head) {
+    Node *cur = head->next;
+
+    while (cur != NULL) {
+        printf("%d ", cur->data);
+        cur = cur->next;
+    }
+}
+
+void freeAll(Node *head) {
+    Node *cur = head->next;
+
+    while (cur != NULL) {
+        Node *next = cur->next;
+        free(cur);
+        cur = next;
+    }
+    head->next = NULL;
 }
 
 int main(void) {
-    head = assign();
-    Node *node1 = assign();
-    head->next = node1;
-    node1->data = 1;
-    Node *node2 = assign();
-    node2->data = 2;
-    node1->next = node2;
+    Node *head = assign();
 
-    Node* cur = head->next;
-
-    while (cur != NULL) {
-        printf("%d\n", cur->data);
-        cur = cur->next;
-    }
+    addFront(head, 1);
+    addFront(head, 2);
+    addFront(head, 3);
+    addFront(head, 4);
+    addFront(head, 5);
+    showAll(head);
+    freeAll(head);
+    showAll(head);
 
     return 0;
 }
